c218637e560f66f6e4959f911e392656cc343e7b
iLikeTrioxin <dziedzickamil91@gmail.com>
2021-11-25 23:00:04 +0100

    Some tweaks (still doesn't build)

--- Engine/Source/Renderer/RendererResourceCreator.h
+++ Engine/Source/Renderer/RendererResourceCreator.h
@@ -14,16 +14,18 @@
 
 namespace PetrolEngine {
 
+    using VL = PetrolEngine::VertexLayout;
+
     class RendererResourceCreator {
 	public:
-		virtual VertexBuffer* newVertexBuffer(const VertexLayout& layout, const void* data, int64 size) = 0;
-		virtual IndexBuffer * newIndexBuffer (                            const void* data, int64 size) = 0;
-		virtual VertexArray * newVertexArray (                                                        ) = 0;
-		virtual Renderer    * newRenderer    (                                                        ) = 0;
+		virtual VertexBuffer* newVertexBuffer(const VL& layout, const void* data, int64 size) = 0;
+		virtual IndexBuffer * newIndexBuffer (                  const void* data, int64 size) = 0;
+		virtual VertexArray * newVertexArray (                                              ) = 0;
+		virtual Renderer    * newRenderer    (                                              ) = 0;
 
-        virtual Shader* newShader( String&& vertexShader  ,
-                                   String&& fragmentShader,
-                                   String&& geometryShader  ) = 0;
+        virtual Shader* newShader( const String& vertexShader  ,
+                                   const String& fragmentShader,
+                                   const String& geometryShader  ) = 0;
 
         virtual Texture* newTexture(const Image* image) = 0;
         virtual Texture* newTexture(int width, int height, TextureFormat format) = 0;
